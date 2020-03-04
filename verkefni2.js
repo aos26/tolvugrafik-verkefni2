ac6fcdab16f4b5e2274910c46537f8c748b018a0
Alexander Þór <athh15@hi.is>
2020-03-04 19:11:32 +0000

    laga liti

--- verkefni2.js
+++ verkefni2.js
@@ -13,6 +13,9 @@ var NumVertices = 24;
 var points = [];
 var colors = [];
 
+var pointsSnake = [];
+var colorsSnake = [];
+
 var vBuffer;
 var vPosition;
 
@@ -22,13 +25,13 @@ var spinY = 0;
 var origX;
 var origY;
 
-var zDist = -3.0;
+var zDist = -5.0;
 var eyesep = 0.2;
 
 var proLoc;
 var mvLoc;
 
-// the 8 vertices of the cube
+/*/ the 8 vertices of the cube
 var v = [
     vec3(-0.5, -0.5, 0.5),
     vec3(-0.5, 0.5, 0.5),
@@ -38,6 +41,17 @@ var v = [
     vec3(-0.5, 0.5, -0.5),
     vec3(0.5, 0.5, -0.5),
     vec3(0.5, -0.5, -0.5)
+];*/
+
+var v = [
+    vec3(-1.0, -1.0, 1.0),
+    vec3(-1.0, 1.0, 1.0),
+    vec3(1.0, 1.0, 1.0),
+    vec3(1.0, -1.0, 1.0),
+    vec3(-1.0, -1.0, -1.0),
+    vec3(-1.0, 1.0, -1.0),
+    vec3(1.0, 1.0, -1.0),
+    vec3(1.0, -1.0, -1.0)
 ];
 
 var lines = [v[0], v[1], v[1], v[2], v[2], v[3], v[3], v[0],
@@ -51,8 +65,10 @@ window.onload = function init() {
     gl = WebGLUtils.setupWebGL(canvas);
     if (!gl) { alert("WebGL isn't available"); }
 
+    colorCube();
+
     gl.viewport(0, 0, canvas.width, canvas.height);
-    gl.clearColor(1.0, 1.0, 1.0, 1.0);
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
 
     gl.enable(gl.DEPTH_TEST);
 
@@ -65,6 +81,18 @@ window.onload = function init() {
     vBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
     gl.bufferData(gl.ARRAY_BUFFER, flatten(lines), gl.STATIC_DRAW);
+
+    /*/ SNAKE
+    var snakeColorBuffer = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, snakeColorBuffer );
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(colorsSnake), gl.STATIC_DRAW );
+
+    var snakeBuffer = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, snakeBuffer );
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(pointsSnake), gl.STATIC_DRAW );
+
+    //************************************************************ */
+
 
     vPosition = gl.getAttribLocation(program, "vPosition");
     gl.vertexAttribPointer(vPosition, 3, gl.FLOAT, false, 0, 0);
@@ -123,11 +151,64 @@ window.onload = function init() {
     render();
 }
 
+function colorCube()
+{
+    quad( 1, 0, 3, 2 );
+    quad( 2, 3, 7, 6 );
+    quad( 3, 0, 4, 7 );
+    quad( 6, 5, 1, 2 );
+    quad( 4, 5, 6, 7 );
+    quad( 5, 4, 0, 1 );
+}
+
+function quad(a, b, c, d) 
+{
+    var vertices = [
+        vec3( -0.5, -0.5,  0.5 ),
+        vec3( -0.5,  0.5,  0.5 ),
+        vec3(  0.5,  0.5,  0.5 ),
+        vec3(  0.5, -0.5,  0.5 ),
+        vec3( -0.5, -0.5, -0.5 ),
+        vec3( -0.5,  0.5, -0.5 ),
+        vec3(  0.5,  0.5, -0.5 ),
+        vec3(  0.5, -0.5, -0.5 )
+    ];
+
+    var vertexColors = [
+        [ 0.0, 0.0, 0.0, 1.0 ],  // black
+        [ 1.0, 0.0, 0.0, 1.0 ],  // red
+        [ 1.0, 1.0, 0.0, 1.0 ],  // yellow
+        [ 0.0, 1.0, 0.0, 1.0 ],  // green
+        [ 0.0, 0.0, 1.0, 1.0 ],  // blue
+        [ 1.0, 0.0, 1.0, 1.0 ],  // magenta
+        [ 0.0, 1.0, 1.0, 1.0 ],  // cyan
+        [ 1.0, 1.0, 1.0, 1.0 ]   // white
+    ];
+
+    // We need to parition the quad into two triangles in order for
+    // WebGL to be able to render it.  In this case, we create two
+    // triangles from the quad indices
+    
+    //vertex color assigned by the index of the vertex
+    
+    var indices = [ a, b, c, a, c, d ];
+
+    for ( var i = 0; i < indices.length; ++i ) {
+        pointsSnake.push( vertices[indices[i]] );
+        //colors.push( vertexColors[indices[i]] );
+    
+        // for solid colored faces use 
+        colorsSnake.push(vertexColors[a]);
+        
+    }
+}
+
+
 function render() {
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
     // Vinstra auga...
-    var mv = lookAt(vec3(0.0 - eyesep / 2.0, 0.0, zDist),
+    var mv = lookAt(vec3(0.0 / 2.0, 0.0, zDist),
         vec3(0.0, 0.0, zDist + 2.0),
         vec3(0.0, 1.0, 0.0));
     mv = mult(mv, mult(rotateX(spinX), rotateY(spinY)));
@@ -137,16 +218,18 @@ function render() {
      gl.uniformMatrix4fv(mvLoc, false, flatten(mv));
      gl.drawArrays(gl.LINES, 0, NumVertices);
  */
-    // Hægra auga...
+    /*/ Hægra auga...
     mv = lookAt(vec3(0.0 + eyesep / 2.0, 0.0, zDist),
         vec3(0.0, 0.0, zDist + 2.0),
         vec3(0.0, 1.0, 0.0));
     mv = mult(mv, mult(rotateX(spinX), rotateY(spinY)));
-
+*/
     // Hægri mynd er í grænbláu (cyan)...
-    gl.uniform4fv(colorLoc, vec4(0.0, 1.0, 1.0, 1.0));
+    gl.uniform4fv(colorLoc, vec4(0.0, 1.0, 0.0, 1.0));
     gl.uniformMatrix4fv(mvLoc, false, flatten(mv));
     gl.drawArrays(gl.LINES, 0, NumVertices);
+
+    
 
     requestAnimFrame(render);
 }
