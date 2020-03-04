772cbda54515c534f8d85c731fe685c0ff940e21
Andrea <aos26@hi.is>
2020-03-04 18:01:05 +0000

    breytt beinagrind

--- verkefni2.js
+++ verkefni2.js
@@ -1,16 +1,20 @@
 /////////////////////////////////////////////////////////////////
 //    Sýnidæmi í Tölvugrafík
-//     Jörðin (sem teningur!) snýst um sólina (stærri teningur!)
+//     Wíragrindarteningur teiknaður tvisvar frá mismunandi
+//     sjónarhorni til að fá víðsjónaráhrif (með gleraugum)
 //
 //    Hjálmtýr Hafsteinsson, febrúar 2020
 /////////////////////////////////////////////////////////////////
 var canvas;
 var gl;
 
-var numVertices = 36;
+var NumVertices = 24;
 
 var points = [];
 var colors = [];
+
+var vBuffer;
+var vPosition;
 
 var movement = false;     // Do we rotate?
 var spinX = 0;
@@ -18,26 +22,37 @@ var spinY = 0;
 var origX;
 var origY;
 
-var rotYear = 0.0;
-var rotYearP = 0.0;
-var rotYearS = 0.0;
+var zDist = -3.0;
+var eyesep = 0.2;
 
-var rotDay = 0.0;
-var earthTilt = 23.5;
+var proLoc;
+var mvLoc;
 
-var matrixLoc;
+// the 8 vertices of the cube
+var v = [
+    vec3(-0.5, -0.5, 0.5),
+    vec3(-0.5, 0.5, 0.5),
+    vec3(0.5, 0.5, 0.5),
+    vec3(0.5, -0.5, 0.5),
+    vec3(-0.5, -0.5, -0.5),
+    vec3(-0.5, 0.5, -0.5),
+    vec3(0.5, 0.5, -0.5),
+    vec3(0.5, -0.5, -0.5)
+];
 
-var start = true;
+var lines = [v[0], v[1], v[1], v[2], v[2], v[3], v[3], v[0],
+v[4], v[5], v[5], v[6], v[6], v[7], v[7], v[4],
+v[0], v[4], v[1], v[5], v[2], v[6], v[3], v[7]
+];
+
 window.onload = function init() {
     canvas = document.getElementById("gl-canvas");
 
     gl = WebGLUtils.setupWebGL(canvas);
     if (!gl) { alert("WebGL isn't available"); }
 
-    colorCube();
-
     gl.viewport(0, 0, canvas.width, canvas.height);
-    gl.clearColor(0.9, 1.0, 1.0, 1.0);
+    gl.clearColor(1.0, 1.0, 1.0, 1.0);
 
     gl.enable(gl.DEPTH_TEST);
 
@@ -47,23 +62,21 @@ window.onload = function init() {
     var program = initShaders(gl, "vertex-shader", "fragment-shader");
     gl.useProgram(program);
 
-    var cBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, flatten(colors), gl.STATIC_DRAW);
-
-    var vColor = gl.getAttribLocation(program, "vColor");
-    gl.vertexAttribPointer(vColor, 4, gl.FLOAT, false, 0, 0);
-    gl.enableVertexAttribArray(vColor);
-
-    var vBuffer = gl.createBuffer();
+    vBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, flatten(points), gl.STATIC_DRAW);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(lines), gl.STATIC_DRAW);
 
-    var vPosition = gl.getAttribLocation(program, "vPosition");
+    vPosition = gl.getAttribLocation(program, "vPosition");
     gl.vertexAttribPointer(vPosition, 3, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(vPosition);
 
-    matrixLoc = gl.getUniformLocation(program, "rotation");
+    colorLoc = gl.getUniformLocation(program, "wireColor");
+
+    proLoc = gl.getUniformLocation(program, "projection");
+    mvLoc = gl.getUniformLocation(program, "modelview");
+
+    var proj = perspective(50.0, 1.0, 0.2, 100.0);
+    gl.uniformMatrix4fv(proLoc, false, flatten(proj));
 
     //event listeners for mouse
     canvas.addEventListener("mousedown", function (e) {
@@ -79,130 +92,61 @@ window.onload = function init() {
 
     canvas.addEventListener("mousemove", function (e) {
         if (movement) {
-            spinY = (spinY + (e.offsetX - origX)) % 360;
+            spinY = (spinY + (origX - e.offsetX)) % 360;
             spinX = (spinX + (e.offsetY - origY)) % 360;
             origX = e.offsetX;
             origY = e.offsetY;
         }
     });
 
+    // Event listener for keyboard
+    window.addEventListener("keydown", function (e) {
+        switch (e.keyCode) {
+            case 38:	// upp ör
+                zDist += 0.1;
+                break;
+            case 40:	// niður ör
+                zDist -= 0.1;
+                break;
+        }
+    });
+
+    // Event listener for mousewheel
+    window.addEventListener("mousewheel", function (e) {
+        if (e.wheelDelta > 0.0) {
+            zDist += 0.1;
+        } else {
+            zDist -= 0.1;
+        }
+    });
+
     render();
 }
-
-function colorCube() {
-    quad(1, 0, 3, 2);
-    quad(2, 3, 7, 6);
-    quad(3, 0, 4, 7);
-    quad(6, 5, 1, 2);
-    quad(4, 5, 6, 7);
-    quad(5, 4, 0, 1);
-}
-
-function quad(a, b, c, d) {
-    var vertices = [
-        vec3(-0.5, -0.5, 0.5),
-        vec3(-0.5, 0.5, 0.5),
-        vec3(0.5, 0.5, 0.5),
-        vec3(0.5, -0.5, 0.5),
-        vec3(-0.5, -0.5, -0.5),
-        vec3(-0.5, 0.5, -0.5),
-        vec3(0.5, 0.5, -0.5),
-        vec3(0.5, -0.5, -0.5)
-    ];
-
-    var vertexColors = [
-        [0.0, 0.0, 0.0, 1.0],  // black
-        [1.0, 0.0, 0.0, 1.0],  // red
-        [1.0, 1.0, 0.0, 1.0],  // yellow
-        [0.0, 1.0, 0.0, 1.0],  // green
-        [0.0, 0.0, 1.0, 1.0],  // blue
-        [1.0, 0.0, 1.0, 1.0],  // magenta
-        [0.0, 1.0, 1.0, 1.0],  // cyan
-        [1.0, 1.0, 1.0, 1.0]   // white
-    ];
-
-    // We need to parition the quad into two triangles in order for
-    // WebGL to be able to render it.  In this case, we create two
-    // triangles from the quad indices
-
-    //vertex color assigned by the index of the vertex
-
-    var indices = [a, b, c, a, c, d];
-
-    for (var i = 0; i < indices.length; ++i) {
-        points.push(vertices[indices[i]]);
-        colors.push(vertexColors[a]);
-
-    }
-
-}
-
 
 function render() {
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-    rotDay += 3.0;
-    rotYear += 0.9;        // not the correct value, but looks better!
-    rotYearP -= 1.1;
-    rotYearS += 0.5;
+    // Vinstra auga...
+    var mv = lookAt(vec3(0.0 - eyesep / 2.0, 0.0, zDist),
+        vec3(0.0, 0.0, zDist + 2.0),
+        vec3(0.0, 1.0, 0.0));
+    mv = mult(mv, mult(rotateX(spinX), rotateY(spinY)));
 
-    var mvstack = [];
+    // Vinstri mynd er í rauðu...
+    /* gl.uniform4fv(colorLoc, vec4(1.0, 0.0, 0.0, 1.0));
+     gl.uniformMatrix4fv(mvLoc, false, flatten(mv));
+     gl.drawArrays(gl.LINES, 0, NumVertices);
+ */
+    // Hægra auga...
+    mv = lookAt(vec3(0.0 + eyesep / 2.0, 0.0, zDist),
+        vec3(0.0, 0.0, zDist + 2.0),
+        vec3(0.0, 1.0, 0.0));
+    mv = mult(mv, mult(rotateX(spinX), rotateY(spinY)));
 
-    var mv = mat4();
-
-    mv = mult(mv, rotateX(spinX));
-    mv = mult(mv, rotateY(spinY));
-    //mvstack.push(mv);
-
-    /*/ teikna "miðjuna"
-    mv = mult( mv, scalem( 0.0, 0.0, 0.0 ) );
-    gl.uniformMatrix4fv(matrixLoc, false, flatten(mv));
-    gl.drawArrays( gl.TRIANGLES, 0, numVertices );
-    */
-    //mv = mvstack.pop();
-    // teikna sól 1
-    mvstack.push(mv);
-    mv = mult(mv, rotateY(rotYear));
-    mv = mult(mv, translate(-0.3, 0.0, 0.0));
-    //mv = mult( mv, rotateY( rotDay ) );
-    mvstack.push(mv);
-    mv = mult(mv, scalem(0.3, 0.3, 0.3));
-    gl.uniformMatrix4fv(matrixLoc, false, flatten(mv));
-    gl.drawArrays(gl.TRIANGLES, 0, numVertices);
-    mv = mvstack.pop();
-
-    // teikna s-type pláneta
-    mvstack.push(mv);
-    mv = mult(mv, rotateY(rotYearS));
-    mv = mult(mv, translate(0.3, 0.0, 0.0));
-    //mv = mult( mv, rotateY( rotDay ) );
-    mv = mult(mv, scalem(0.1, 0.1, 0.1));
-    gl.uniformMatrix4fv(matrixLoc, false, flatten(mv));
-    gl.drawArrays(gl.TRIANGLES, 0, numVertices);
-    mv = mvstack.pop();
-    mv = mvstack.pop();
-
-
-    // teikna sól 2
-    mvstack.push(mv);
-    mv = mult(mv, rotateY(rotYear));
-    mv = mult(mv, translate(0.3, 0.0, 0.0));
-    //mv = mult( mv, rotateY( rotDay ) );
-    mv = mult(mv, scalem(0.3, 0.3, 0.3));
-    gl.uniformMatrix4fv(matrixLoc, false, flatten(mv));
-    gl.drawArrays(gl.TRIANGLES, 0, numVertices);
-
-    mv = mvstack.pop();
-
-    mvstack.push(mv);
-    //teikna p-type pláneta
-    mv = mult(mv, rotateY(rotYearP));
-    mv = mult(mv, translate(1.0, 0.0, 0.0));
-    //mv = mult( mv, rotateY( rotDay ) );
-    mv = mult(mv, scalem(0.2, 0.2, 0.2));
-    gl.uniformMatrix4fv(matrixLoc, false, flatten(mv));
-    gl.drawArrays(gl.TRIANGLES, 0, numVertices);
-    mv = mvstack.pop();
+    // Hægri mynd er í grænbláu (cyan)...
+    gl.uniform4fv(colorLoc, vec4(0.0, 1.0, 1.0, 1.0));
+    gl.uniformMatrix4fv(mvLoc, false, flatten(mv));
+    gl.drawArrays(gl.LINES, 0, NumVertices);
 
     requestAnimFrame(render);
 }
