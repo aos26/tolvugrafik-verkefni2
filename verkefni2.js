80c2dac1ca0240c0ed5adf43b9bf815d8d35b7d3
Andrea <aos26@hi.is>
2020-03-04 22:27:00 +0000

    snek kominn vantar movement

--- verkefni2.js
+++ verkefni2.js
@@ -15,6 +15,7 @@ var colors = [];
 
 var pointsSnake = [];
 var colorsSnake = [];
+var drawCounter = 1;
 
 var vBuffer;
 var vPosition;
@@ -92,12 +93,13 @@ window.onload = function init() {
 
     var proj = perspective(50.0, 1.0, 0.2, 100.0);
     gl.uniformMatrix4fv(proLoc, false, flatten(proj));
-    
+
     snakeBody.push(Snake(true));
     for (let i = 0; i < 4; i++) {
         snakeBody.push(Snake(false));
     }
-    snakeBody.reverse();
+
+    //snakeBody.reverse();
     console.log(snakeBody)
 
     //event listeners for mouse
@@ -145,61 +147,59 @@ window.onload = function init() {
     render();
 }
 
-function colorCube()
-{
-    quad( 1, 0, 3, 2 );
-    quad( 2, 3, 7, 6 );
-    quad( 3, 0, 4, 7 );
-    quad( 6, 5, 1, 2 );
-    quad( 4, 5, 6, 7 );
-    quad( 5, 4, 0, 1 );
+function colorCube() {
+    quad(1, 0, 3, 2);
+    quad(2, 3, 7, 6);
+    quad(3, 0, 4, 7);
+    quad(6, 5, 1, 2);
+    quad(4, 5, 6, 7);
+    quad(5, 4, 0, 1);
 }
 
-function quad(a, b, c, d) 
-{
+function quad(a, b, c, d) {
     var vertices = [
-        vec3( -0.1, -0.1,  0.1 ),
-        vec3( -0.1,  0.1,  0.1 ),
-        vec3(  0.1,  0.1,  0.1 ),
-        vec3(  0.1, -0.1,  0.1 ),
-        vec3( -0.1, -0.1, -0.1 ),
-        vec3( -0.1,  0.1, -0.1 ),
-        vec3(  0.1,  0.1, -0.1 ),
-        vec3(  0.1, -0.1, -0.1 )
+        vec3(-0.1, -0.1, 0.1),
+        vec3(-0.1, 0.1, 0.1),
+        vec3(0.1, 0.1, 0.1),
+        vec3(0.1, -0.1, 0.1),
+        vec3(-0.1, -0.1, -0.1),
+        vec3(-0.1, 0.1, -0.1),
+        vec3(0.1, 0.1, -0.1),
+        vec3(0.1, -0.1, -0.1)
     ];
 
     var vertexColors = [
-        [ 0.0, 0.0, 0.0, 1.0 ],  // black
-        [ 1.0, 0.0, 0.0, 1.0 ],  // red
-        [ 1.0, 1.0, 0.0, 1.0 ],  // yellow
-        [ 0.0, 1.0, 0.0, 1.0 ],  // green
-        [ 0.0, 0.0, 1.0, 1.0 ],  // blue
-        [ 1.0, 0.0, 1.0, 1.0 ],  // magenta
-        [ 0.0, 1.0, 1.0, 1.0 ],  // cyan
-        [ 1.0, 1.0, 1.0, 1.0 ]   // white
+        [0.0, 0.0, 0.0, 1.0],  // black
+        [1.0, 0.0, 0.0, 1.0],  // red
+        [1.0, 1.0, 0.0, 1.0],  // yellow
+        [0.0, 1.0, 0.0, 1.0],  // green
+        [0.0, 0.0, 1.0, 1.0],  // blue
+        [1.0, 0.0, 1.0, 1.0],  // magenta
+        [0.0, 1.0, 1.0, 1.0],  // cyan
+        [1.0, 1.0, 1.0, 1.0]   // white
     ];
 
     // We need to parition the quad into two triangles in order for
     // WebGL to be able to render it.  In this case, we create two
     // triangles from the quad indices
-    
-    //vertex color assigned by the index of the vertex
-    
-    var indices = [ a, b, c, a, c, d ];
 
-    for ( var i = 0; i < indices.length; ++i ) {
-        pointsSnake.push( vertices[indices[i]] );
+    //vertex color assigned by the index of the vertex
+
+    var indices = [a, b, c, a, c, d];
+
+    for (var i = 0; i < indices.length; ++i) {
+        pointsSnake.push(vertices[indices[i]]);
         //colors.push( vertexColors[indices[i]] );
-    
+
         // for solid colored faces use 
         colorsSnake.push(vertexColors[a]);
-        
+
     }
 }
 
 function Snake(head) {
     let c = vec4(1.0, 0.0, 0.0, 1.0);
-    if(head) {
+    if (head) {
         c = vec4(0.0, 0.0, 1.0, 1.0);
     }
     return {
@@ -208,55 +208,83 @@ function Snake(head) {
             y: 0.0,
             z: 0.0,
         },
-        speed: 0.01,
+        speed: 0.1,
         size: 0.5,
         color: c,
+        head: head, // True ef haus, false annars
     };
 }
 
-function drawSnake(mv, snake, i) {
+function drawSnake(mv, snake) {
     //console.log(snake);
-    mv = mult(mv, translate(snake.pos.x + i, snake.pos.y+ i, snake.pos.z+ i ));
-    mv = mult( mv, scalem (snake.size, snake.size, snake.size ));
+    if (snake.head) {
+        mv = mult(mv, translate(snake.pos.x, snake.pos.y, snake.pos.z));
+    } else {
+        mv = mult(mv, translate(snake.pos.x, snake.pos.y, snake.pos.z));
+    }
+    mv = mult(mv, scalem(snake.size, snake.size, snake.size));
 
-    /*console.log(vertices.length)
-    console.log(NumVerticesSnake)
-    */
-
-    gl.uniform4fv( colorLoc, snake.color); 
+    gl.uniform4fv(colorLoc, snake.color);
     gl.uniformMatrix4fv(mvLoc, false, flatten(mv));
-    gl.drawArrays( gl.TRIANGLES, 0, NumVerticesSnake);//NumVerticesSnake);
+    gl.drawArrays(gl.TRIANGLES, 0, NumVerticesSnake);//NumVerticesSnake);
 }
 
-function updateSnake(snake) {
+function updateSnake(snakeHead, snakeTail) {
+    snakeTail.pos = snakeHead.pos;
+}
+
+function moveSnake(snake) {
     snake.pos = {
-        x: snake.speed,
+        x: snake.pos.x + snake.speed,
         y: 0,
-        y: 0,
+        z: 0,
     };
+    drawCounter = 1;
+    while (drawCounter < snakeBody.length) {
+        for (let i = 1; i < drawCounter; i++) {
+            updateSnake(snakeBody[i - 1], snakeBody[i]);
+        }
+        drawCounter++;
+    }
+
 }
 
 function render() {
-    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+    setTimeout(function () {
+        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-    // Vinstra auga...
-    var mv = lookAt(vec3(0.0 , 0.0, zDist),
-        vec3(0.0, 0.0, zDist + 2.0),
-        vec3(0.0, 1.0, 0.0));
-    mv = mult(mv, mult(rotateX(spinX), rotateY(spinY)));
+        // Vinstra auga...
+        var mv = lookAt(vec3(0.0, 0.0, zDist),
+            vec3(0.0, 0.0, zDist + 2.0),
+            vec3(0.0, 1.0, 0.0));
+        mv = mult(mv, mult(rotateX(spinX), rotateY(spinY)));
 
-    for (let i = 0; i < snakeBody.length; i++) {
-        //updateSnake(snakeBody[i]);
-        drawSnake(mv, snakeBody[i], i*0.1);
-    }
 
-    //mv = mult( mv, scalem ( (15), (15), (15) ) );
-    // Teikna kassa
-    gl.uniform4fv(colorLoc, vec4(0.0, 1.0, 0.0, 1.0));
-    gl.uniformMatrix4fv(mvLoc, false, flatten(mv));
-    gl.drawArrays(gl.LINES, NumVerticesSnake, NumVerticesBox);
 
-    
+        moveSnake(snakeBody[0]);
+        /*
+        if (drawCounter > snakeBody.length) {
+            drawCounter = 1;
+        }
+        for (let i = 1; i < drawCounter; i++) {
+            updateSnake(snakeBody[i - 1], snakeBody[i]);
+        }
+        drawCounter++;
+        */
 
-    requestAnimFrame(render);
+
+
+        for (let i = 0; i < snakeBody.length; i++) {
+            drawSnake(mv, snakeBody[i]);
+        }
+        //mv = mult( mv, scalem ( (15), (15), (15) ) );
+        // Teikna kassa
+        gl.uniform4fv(colorLoc, vec4(0.0, 1.0, 0.0, 1.0));
+        gl.uniformMatrix4fv(mvLoc, false, flatten(mv));
+        gl.drawArrays(gl.LINES, NumVerticesSnake, NumVerticesBox);
+
+
+        window.requestAnimFrame(render);
+    }, 60);
+    //requestAnimFrame(render);
 }
